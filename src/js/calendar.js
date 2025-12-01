fc0306ed766ef6fa37947376d99f0f82458841d8
Eduardo de Almeida Fernandes <eduardo.fernandes@vexpenses.com>
2025-11-30 22:05:24 -0300

    refactor: refatora as telas usando mais o daisyUI e as regras de design system

---
+++ src/js/calendar.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const callyWidget = document.getElementById('cally-widget');
+  const displaySpan = document.getElementById('data-display');
+  const hiddenInput = document.getElementById('data-input');
+  const btnLimpar = document.getElementById('btn-limpar');
+
+  if (callyWidget) {
+    callyWidget.addEventListener('change', (event) => {
+      const selectedDate = event.target.value;
+      
+      hiddenInput.value = selectedDate;
+      const [year, month, day] = selectedDate.split('-');
+      displaySpan.textContent = `${day}/${month}/${year}`;
+      
+      displaySpan.classList.remove('text-zinc-400');
+      displaySpan.classList.add('text-zinc-950');
+
+      btnLimpar.classList.remove('hidden');
+
+      if (document.activeElement instanceof HTMLElement) {
+        document.activeElement.blur();
+      }
+    });
+
+    btnLimpar.addEventListener('mousedown', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+    });
+
+    btnLimpar.addEventListener('click', (e) => {
+      e.stopPropagation(); 
+      e.preventDefault();
+
+      callyWidget.value = '';
+      hiddenInput.value = '';
+      
+      displaySpan.textContent = 'Selecionar data...';
+      displaySpan.classList.remove('text-zinc-950');
+      displaySpan.classList.add('text-zinc-400');
+
+      btnLimpar.classList.add('hidden');
+    });
+  }
+});
